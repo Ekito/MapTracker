6d10ef32a833b65a17daac400c8b75c695b7c9e6
ndeverge <ndeverge@ekito.fr>
2012-04-30 14:43:34 +0200

    A test with 2 vehicles

--- test/integrationTest/MovingVehicle.java
+++ test/integrationTest/MovingVehicle.java
@@ -29,44 +29,71 @@ public class MovingVehicle {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(testData));
 
+		int cpt = 0;
+
+		// we store the old location for the chaser vehicle
+		List<String> cache = new ArrayList<String>();
+
 		String line;
 		while ((line = br.readLine()) != null) {
 			String[] split = line.split(";");
 			if (split.length > 1) {
-				String longitude = split[0];
-				String latitude = split[1];
 
-				// Since logger are disable in Play! tests
-				// (https://groups.google.com/forum/#!msg/play-framework/ydU081RerA8/zueBL2ePJgcJ)
-				// we use a System.out.println
-				System.out.println("Moving to " + longitude + ", " + latitude);
+				cache.add(line);
 
-				HttpClient client = new DefaultHttpClient();
+				move("Oh no, I'm being followed !!", split);
 
-				HttpPost post = new HttpPost("http://localhost:9000/moveTo");
-				try {
-					List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(
-							1);
-					nameValuePairs.add(new BasicNameValuePair("id",
-							"the vehicle"));
-					nameValuePairs.add(new BasicNameValuePair("timestamp",
-							String.valueOf(System.currentTimeMillis())));
-					nameValuePairs.add(new BasicNameValuePair("longitude",
-							longitude));
-					nameValuePairs.add(new BasicNameValuePair("latitude",
-							latitude));
-
-					post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-
-					client.execute(post);
-
-				} catch (IOException e) {
-					e.printStackTrace();
+				// move the second vehicle ("the chaser")
+				// we let 20 steps ahead
+				if (cpt > 20) {
+					String chaserPosition = cache.get(cpt - 20);
+					move("I'll catch you !!", chaserPosition.split(";"));
 				}
 
 				Thread.sleep(100);
 			}
+			cpt++;
 
+		}
+
+	}
+
+	/**
+	 * Move a vehicle
+	 * 
+	 * @param vehicleId
+	 *            the vehicle to move
+	 * @param longLat
+	 *            the new position
+	 */
+	private void move(String vehicleId, String[] longLat) {
+
+		String longitude = longLat[0];
+		String latitude = longLat[1];
+
+		// Since logger are disable in Play! tests
+		// (https://groups.google.com/forum/#!msg/play-framework/ydU081RerA8/zueBL2ePJgcJ)
+		// we use a System.out.println
+		System.out.println("Moving '" + vehicleId + "' to " + longitude + ", "
+				+ latitude);
+
+		HttpClient client = new DefaultHttpClient();
+
+		HttpPost post = new HttpPost("http://localhost:9000/moveTo");
+		try {
+			// move the first vehicle ("the victim")
+			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+			nameValuePairs.add(new BasicNameValuePair("id", vehicleId));
+			nameValuePairs.add(new BasicNameValuePair("timestamp", String
+					.valueOf(System.currentTimeMillis())));
+			nameValuePairs.add(new BasicNameValuePair("longitude", longitude));
+			nameValuePairs.add(new BasicNameValuePair("latitude", latitude));
+
+			post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+			client.execute(post);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
 	}
